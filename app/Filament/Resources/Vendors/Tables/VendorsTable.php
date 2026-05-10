20584299457720b5dadf93ffcb646bd67b050c78
Ahmed Kalalsh <Ahmedkalash513@gmail.com>
2026-05-10 16:18:15 +0300

    feat(vendors-table): add delete action to record actions

--- app/Filament/Resources/Vendors/Tables/VendorsTable.php
+++ app/Filament/Resources/Vendors/Tables/VendorsTable.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Vendors\Tables;
 
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Support\Enums\Width;
@@ -94,6 +95,7 @@ class VendorsTable
             ->recordActions([
                 EditAction::make()
                 ->modalWidth(Width::FiveExtraLarge),
+                DeleteAction::make()
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
